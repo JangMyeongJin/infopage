253d86322ec60aaec52ed7829ae15530a1ece64a
jean9710 <jean9710@proten.co.kr>
2024-11-14 13:15:59 +0900

    문의사항 추가

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
     return (
         <footer className="container">
-            <p>문의사항: jean9710@proten.co.kr</p>
+            <p>문의사항: 동호회 회장 장명진 (jean9710@proten.co.kr)</p>
         </footer>
     )
 }
